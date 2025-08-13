89a4cc46ff6f226f0531c600f4cd23e286351132
Dev-Abhi1 <Dev-Abhi1>
2025-08-13 20:44:34 +0530

    Register form ui done

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const indexRoutes = require("./routes/index.routes");
-
+const authRoutes = require("./routes/auth.routes")
 const app = express()
 
 app.set("view engine","ejs");
@@ -9,5 +9,7 @@ app.use(express.static('public')) // serve static files from the public director
 app.use(express.json()) // used when we send and need to read data in raw format
 app.use(express.urlencoded({extended:true})) // used when we  need to send and read data in x-www-form-unlencoded
 app.use("/",indexRoutes)
+app.use("/auth",authRoutes)
+
 
 module.exports = app
